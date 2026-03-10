1c79678ed364c037751bcbaeca3e749c3b8b6cfd
shikhabista <shikhabista33@gmail.com>
2026-03-10 12:40:16 +0545

    chore: user login

---
+++ Dotnet-Mvc/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Claims;
+using Dotnet_Mvc.Services.Interface;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Mvc.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IUserService _userService;
+
+    public AuthController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(string username, string password)
+    {
+        try
+        {
+            var user =  _userService.GetUser(username, password);
+            
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim("UserId", user.Id.ToString()),
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("Index", "Home");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
---
+++ Dotnet-Mvc/ViewModel/LoginVm.cs
@@ -0,0 +1,7 @@
+﻿namespace Dotnet_Mvc.ViewModel;
+
+public class LoginVm
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
