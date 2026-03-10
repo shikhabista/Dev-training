9efb96e253df3e5e4e7240a58aa4355444fe0f71
shikhabista <shikhabista33@gmail.com>
2026-03-10 11:23:55 +0545

    chore: migrations

---
+++ Dotnet-Mvc/Migrations/20260310051300_Initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Dotnet_Mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "admin");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                schema: "admin",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    address = table.Column<string>(type: "text", nullable: true),
+                    rec_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    rec_status = table.Column<char>(type: "character(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user",
+                schema: "admin");
+        }
+    }
+}
---
+++ Dotnet-Mvc/Migrations/20260310053818_UserModelUpdate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dotnet_Mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "contact_no",
+                schema: "admin",
+                table: "user",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                schema: "admin",
+                table: "user",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "contact_no",
+                schema: "admin",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                schema: "admin",
+                table: "user");
+        }
+    }
+}
